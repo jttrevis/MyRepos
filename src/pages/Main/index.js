38f3177052241739543404701d295f1567afc994
jttrevis <jr.trevis@hotmail.com>
2022-11-08 14:41:18 +0000

    added callback api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
 import { Container, Form, SubmitButton } from './styles';
 import { FaGithub, FaPlus } from 'react-icons/fa';
+
+import { api } from '../../services/api';
+
 export const Main = () => {
-  return (
-    <Container>
-      <h1><FaGithub size={25} />Main</h1>
+	const [newRepo, setNewRepo] = useState('');
+	const [repositories, setRepositories] = useState([]);
+	function handleInputChange(e) {
+		setNewRepo(e.target.value);
+	}
 
+	const hanldeSubmit = useCallback(
+		(e) => {
+			e.preventDefault();
+			async function submit() {
+				const response = await api.get(`repos/${newRepo}`);
+				const data = {
+					name: response.data.full_name,
+				};
 
-      <Form onSubmit={() => { }}>
-        <input type="text" placeholder='Add Repository' />
+				setRepositories([...repositories, data]);
+				setNewRepo('');
+			}
 
+			submit();
+		},
+		[newRepo, repositories],
+	);
 
-        <SubmitButton>
-          <FaPlus color='#fff' size={14} />
-        </SubmitButton>
-      </Form>
+	return (
+		<Container>
+			<h1>
+				<FaGithub size={25} />
+				Main
+			</h1>
 
-    </Container>
-  );
+			<Form onSubmit={hanldeSubmit}>
+				<input
+					type='text'
+					placeholder='Add Repository'
+					value={newRepo}
+					onChange={handleInputChange}
+				/>
+
+				<SubmitButton>
+					<FaPlus
+						color='#fff'
+						size={14}
+					/>
+				</SubmitButton>
+			</Form>
+		</Container>
+	);
 };
