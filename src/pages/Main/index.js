1690a23edae02bbd557d77600bad4f90fa036a4b
jttrevis <jr.trevis@hotmail.com>
2022-11-08 15:10:24 +0000

    list on main page added

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,13 @@
 import { useState, useCallback } from 'react';
-import { Container, Form, SubmitButton } from './styles';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 
 import { api } from '../../services/api';
 
 export const Main = () => {
 	const [newRepo, setNewRepo] = useState('');
 	const [repositories, setRepositories] = useState([]);
+	const [loanding, setLoanding] = useState(false);
 	function handleInputChange(e) {
 		setNewRepo(e.target.value);
 	}
@@ -14,19 +15,35 @@ export const Main = () => {
 	const hanldeSubmit = useCallback(
 		(e) => {
 			e.preventDefault();
-			async function submit() {
-				const response = await api.get(`repos/${newRepo}`);
-				const data = {
-					name: response.data.full_name,
-				};
 
-				setRepositories([...repositories, data]);
-				setNewRepo('');
+			async function submit() {
+				setLoanding(true);
+				try {
+					const response = await api.get(`repos/${newRepo}`);
+					const data = {
+						name: response.data.full_name,
+					};
+
+					setRepositories([...repositories, data]);
+					setNewRepo('');
+				} catch (err) {
+					console.log(err);
+				} finally {
+					setLoanding(false);
+				}
 			}
 
 			submit();
 		},
 		[newRepo, repositories],
+	);
+
+	const hanldeDelete = useCallback(
+		(repo) => {
+			const find = repositories.filter((r) => r.name !== repo);
+			setRepositories(find);
+		},
+		[repositories],
 	);
 
 	return (
@@ -44,13 +61,36 @@ export const Main = () => {
 					onChange={handleInputChange}
 				/>
 
-				<SubmitButton>
-					<FaPlus
-						color='#fff'
-						size={14}
-					/>
+				<SubmitButton loanding={loanding ? 1 : 0}>
+					{loanding ? (
+						<FaSpinner
+							color='#fff'
+							size={14}
+						/>
+					) : (
+						<FaPlus
+							color='#fff'
+							size={14}
+						/>
+					)}
 				</SubmitButton>
 			</Form>
+
+			<List>
+				{repositories.map((repo) => (
+					<li key={repo.name}>
+						<span>
+							<DeleteButton onClick={() => hanldeDelete(repo.name)}>
+								<FaTrash size={14} />
+							</DeleteButton>
+							{repo.name}
+						</span>
+						<a href=''>
+							<FaBars size={20} />
+						</a>
+					</li>
+				))}
+			</List>
 		</Container>
 	);
 };
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,50 +1,106 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0,0,0, 0.2);
-  padding: 30px;
-  margin: 80px auto;
+	max-width: 700px;
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+	padding: 30px;
+	margin: 80px auto;
 
+	h1 {
+		font-size: 20px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
 
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    
-    svg {
-      margin-right: 10px;
-    }
-  }
-
+		svg {
+			margin-right: 10px;
+		}
+	}
 `;
 
 export const Form = styled.form`
-  margin-top: 30px;
-  display: flex;
-  flex-direction: row;
+	margin-top: 30px;
+	display: flex;
+	flex-direction: row;
 
-  input {
-    flex: 1;
-    border: 1px solid #ddd;
-    padding: 10px 15px;
-    border-radius: 4px;
-    font-size: 17px;
+	input {
+		flex: 1;
+		border: 1px solid #ddd;
+		padding: 10px 15px;
+		border-radius: 4px;
+		font-size: 17px;
+	}
+`;
+
+// Animation button
+const animate = keyframes`
+  from{
+    transform: rotate(0deg);
   }
-`
 
-export const SubmitButton = styled.button.attrs({
-  type: 'submit'
+  to{
+    transform: rotate(360deg);
+  }
+
+`;
+export const SubmitButton = styled.button.attrs((props) => ({
+	type: 'submit',
+	disable: props.loading,
+}))`
+	background: #0d2636;
+	border: 0;
+	border-radius: 4px;
+	margin-left: 10px;
+	padding: 0 15px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	&[disabled] {
+		cursor: not-allowed;
+		opacity: 0.5;
+	}
+
+	${(props) =>
+		props.loanding &&
+		css`
+			svg {
+				animation: ${animate} 2s linear infinite;
+			}
+		`}
+`;
+
+export const List = styled.ul`
+	list-style: none;
+	margin-top: 20px;
+
+	li {
+		padding: 15px 0;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+
+		& + li {
+			border-top: 1px solid #eee;
+		}
+
+		a {
+			color: #0d2636;
+			text-decoration: none;
+		}
+	}
+`;
+
+export const DeleteButton = styled.button.attrs({
+	type: 'button',
 })`
-  background: #0d2636;
-  border: 0;
-  border-radius: 4px;
-  margin-left: 10px;
-  padding: 0 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+	background: transparent;
+	color: #0d2636;
+	border: 0;
+	padding: 8px 7px;
+	outline: 0;
+	border-radius: 4px;
+`;
