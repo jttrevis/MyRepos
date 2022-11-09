025867acfc3d0cd10cc3319b1416bb90839dad09
jttrevis <jr.trevis@hotmail.com>
2022-11-09 21:01:23 +0000

    added pagination on issues page

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react';
 import { api } from '../../services/api';
 import { useParams } from 'react-router-dom';
-import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
+import {
+	Container,
+	Owner,
+	Loading,
+	BackButton,
+	IssuesList,
+	PageActions,
+} from './styles';
 
 export const Repository = ({ match }) => {
 	const { repository } = useParams();
@@ -10,26 +17,44 @@ export const Repository = ({ match }) => {
 	const [repo, setRepo] = useState({});
 	const [issues, setIssues] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [page, setPage] = useState(1);
 	useEffect(() => {
 		async function load() {
 			const [repoData, issuesData] = await Promise.all([
 				api.get(`/repos/${repository}`),
-				api.get(`/repos/${repository}/issues`),
-				{
+				api.get(`/repos/${repository}/issues`, {
 					params: {
 						state: 'open',
+						per_page: 5,
 					},
-				},
+				}),
 			]);
 
 			setRepo(repoData.data);
 			setIssues(issuesData.data);
-			console.log(issuesData.data);
 			setLoading(false);
 		}
 
 		load();
 	}, [repository]);
+
+	function handlePage(action) {
+		setPage(action === 'prev' ? page - 1 : page + 1);
+	}
+
+	useEffect(() => {
+		async function loadIssue() {
+			const response = await api.get(`/repos/${repository}/issues`, {
+				params: {
+					state: 'open',
+					page,
+					per_page: 5,
+				},
+			});
+			setIssues(response.data);
+		}
+		loadIssue();
+	}, [page, repository]);
 
 	if (loading) {
 		return (
@@ -75,6 +100,22 @@ export const Repository = ({ match }) => {
 					</li>
 				))}
 			</IssuesList>
+
+			<PageActions>
+				<button
+					type='button'
+					onClick={() => handlePage('prev')}
+					disabled={page < 2}
+				>
+					Prev
+				</button>
+				<button
+					type='button'
+					onClick={() => handlePage('next')}
+				>
+					Next
+				</button>
+			</PageActions>
 		</Container>
 	);
 };
